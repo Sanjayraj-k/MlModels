8bc3d436f8b6c3f4484832c50db1ddfaf7f9af53
SanjayK-123 <155231012+SanjayK-123@users.noreply.github.com>
2024-12-30 14:54:29 +0530

    jiy

--- app/chat.py
+++ app/chat.py
@@ -1,10 +1,9 @@
 from streamlit_option_menu import option_menu
 import streamlit as st
 import joblib
-import pickle
-
-sentiment_mpdel=pickle.load(open('app/sentiment_model.pkl','rb'))
-vectorizer = joblib.load("/workspaces/MlModels/app/tfidf_vectorizer.pkl")
+import matplotlib.pyplot as plt
+sentiment_mpdel=joblib.load(open('sentiment_model.pkl','rb'))
+vectorizer = joblib.load("tfidf_vectorizer.pkl")
 with st.sidebar:
     st.title("Machine Learning Models")
     selected = option_menu(
@@ -16,15 +15,43 @@ if selected=="Sentiment Analysis":
     st.title("Sentiment Analysis")
     text=st.text_area("Enter the Sentence to predict the sentiment")
     if st.button("Predict"):
-        loaded_model = joblib.load("/workspaces/MlModels/app/sentiment_model.pkl")
-        vectorizer = joblib.load("/workspaces/MlModels/app/tfidf_vectorizer.pkl")
+        loaded_model = joblib.load("sentiment_model.pkl")
+        vectorizer = joblib.load("tfidf_vectorizer.pkl")
     #processed_input = clean_text(text)
     #vectorized_input = vectorizer.transform([processed_input])
         prediction = loaded_model.predict(vectorizer.transform([text]))
 
         st.success((f"Prediction: {prediction[0]}"))
-        
-        
+        if prediction[0]=='positive':
+            pos=100
+            neg=0
+            neu=0
+            sent={'positive':pos,'negative':neg,'neutral':neu}
+            plt.bar(sent.keys(),sent.values())
+            plt.xlabel('Sentiments')    
+            plt.ylabel('Count') 
+            plt.title('Sentiment Analysis')
+            st.pyplot(plt)
+        elif prediction[0]=="negative":
+            pos=0
+            neg=100
+            neu=0
+            sent={'positive':pos,'negative':neg,'neutral':neu}
+            plt.bar(sent.keys(),sent.values())
+            plt.xlabel('Sentiments')    
+            plt.ylabel('Count') 
+            plt.title('Sentiment Analysis')
+            st.pyplot(plt)
+        else:
+            pos=0
+            neg=0
+            neu=100
+            sent={'positive':pos,'negative':neg,'neutral':neu}
+            plt.bar(sent.keys(),sent.values())
+            plt.xlabel('Sentiments')    
+            plt.ylabel('Count') 
+            plt.title('Sentiment Analysis')
+            st.pyplot(plt)
 else:
     st.title("SleepTime Analysis")
     col1,col2=st.columns(2)
@@ -44,7 +71,7 @@ else:
         RelaxationTime=st.number_input("Enter your Relaxation Time",min_value=0.0,step=0.1,max_value=10.0)
     predict=st.button("Predict Sleep Time")
     if predict:
-        loaded_model = joblib.load("app/sleep.pkl")
+        loaded_model = joblib.load("sleep.pkl")
         prediction = loaded_model.predict([[work,study,travel,workhours,CaffeineIntake,RelaxationTime]])
         st.success(f"Predicted Sleep Time is {prediction[0]} hours")
         sleep={'Sleep Time':prediction[0]}
